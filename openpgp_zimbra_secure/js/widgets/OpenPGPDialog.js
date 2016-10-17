b4f3d1b3023975bcf36459621bb8fad8c4aa7d6e
nguyennv <nguyennv1981@gmail.com>
2016-10-17 23:33:36 +0700

    Add OpenPGPDialog

---
+++ openpgp_zimbra_secure/js/widgets/OpenPGPDialog.js
@@ -0,0 +1,38 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * OpenPGP Zimbra Secure is the open source digital signature and encrypt for Zimbra Collaboration Open Source Edition software
+ * Copyright (C) 2016-present OpenPGP Zimbra Secure
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ * ***** END LICENSE BLOCK *****
+ *
+ * OpenPGP MIME Secure Email Zimlet
+ *
+ * Written by nguyennv1981@gmail.com
+ */
+
+OpenPGPDialog = function(handler, title, okListener, cancelListener, standardButtons) {
+    ZmDialog.call(this, {
+        title: title,
+        parent: handler.getShell(),
+        className: "OpenPGP dialog",
+        standardButtons: standardButtons
+    });
+};
+
+OpenPGPDialog.prototype = new ZmDialog;
+OpenPGPDialog.prototype.constructor = OpenPGPDialog;
+
+OpenPGPDialog.prototype.toString = function() {
+    return "OpenPGPDialog";
+};
