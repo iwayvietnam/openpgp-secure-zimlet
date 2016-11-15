ba8ad67ffe6e705b9a1023e0e8068115c4fc64bc
nguyennv <nguyennv1981@gmail.com>
2016-11-15 18:31:10 +0700

    Add ImportPublicKeyDialog

---
+++ openpgp_zimbra_secure/js/widgets/ImportPublicKeyDialog.js
@@ -0,0 +1,50 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ * OpenPGP Zimbra Secure is the open source digital signature and encrypt for Zimbra Collaboration Open Source Edition software
+ * Copyright (C) 2016-present OpenPGP Zimbra Secure
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ * ***** END LICENSE BLOCK *****
+ *
+ * OpenPGP MIME Secure Email Zimlet
+ *
+ * Written by nguyennv1981@gmail.com
+ */
+
+ImportPublicKeyDialog = function(handler, onOk, onCancel, keyInfo) {
+    OpenPGPDialog.call(
+        this,
+        handler,
+        OpenPGPUtils.prop('importPublicKeyTitle'),
+        onOk,
+        onCancel,
+        [DwtDialog.CANCEL_BUTTON, DwtDialog.OK_BUTTON]
+    );
+    this._keyInfo = keyInfo;
+
+    this.setView(new ImportPublicKeyView({
+        parent: this
+    }, keyInfo));
+};
+
+ImportPublicKeyDialog.prototype = new OpenPGPDialog;
+ImportPublicKeyDialog.prototype.constructor = ImportPublicKeyDialog;
+
+ImportPublicKeyDialog.prototype.toString = function() {
+    return 'ImportPublicKeyDialog';
+};
+
+ImportPublicKeyDialog.prototype.importPubicKey = function(callback) {
+    this._handler._pgpKeys.addPublicKey(key);
+    callback.run();
+};
