2582217b7b5abb87230975529c652feff16c8725
nguyennv <nguyennv1981@gmail.com>
2016-11-16 09:53:21 +0700

    Fix set security image button

--- openpgp_zimbra_secure/js/OpenPGPZimbraSecure.js
+++ openpgp_zimbra_secure/js/OpenPGPZimbraSecure.js
@@ -807,11 +807,17 @@ OpenPGPZimbraSecure.prototype.onSendButtonClicked = function(controller, msg) {
 }
 
 OpenPGPZimbraSecure.prototype._setSecurityImage = function(button, value) {
-    var security_types = [
-        {label: this.getMessage('dontSignMessage'), className: 'DontSign'},
-        {label: this.getMessage('signMessage'), className: 'Sign'},
-        {label: this.getMessage('signAndEncryptMessage'), className: 'SignEncrypt'}
-    ];
+    var security_types = {
+        OpenPGPZimbraSecure.OPENPGP_DONTSIGN: {
+            {label: this.getMessage('dontSignMessage'), className: 'DontSign'}
+        },
+        OpenPGPZimbraSecure.OPENPGP_SIGN: {
+            {label: this.getMessage('signMessage'), className: 'Sign'}
+        },
+        OpenPGPZimbraSecure.OPENPGP_SIGNENCRYPT: {
+            {label: this.getMessage('signAndEncryptMessage'), className: 'SignEncrypt'}
+        }
+    };
 
     button.setImage(security_types[value].className);
     button.setText(security_types[value].label);
