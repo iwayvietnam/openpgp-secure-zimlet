eed54a6cdbf68cd07c8b2131bafc94e793b7cc3b
nguyennv <nguyennv1981@gmail.com>
2016-12-26 15:13:52 +0700

    Fix encrypted status of inline encrypted message

--- openpgp_zimbra_secure/js/OpenPGPDecrypt.js
+++ openpgp_zimbra_secure/js/OpenPGPDecrypt.js
@@ -224,6 +224,7 @@ OpenPGPDecrypt.decryptContent = function(content, publicKeys, privateKey, onDecr
             };
             return openpgp.decrypt(opts).then(function(plainText) {
                 return {
+                    encrypted: true,
                     content: plainText.data,
                     signatures: plainText.signatures
                 };
@@ -231,6 +232,7 @@ OpenPGPDecrypt.decryptContent = function(content, publicKeys, privateKey, onDecr
         }
         else {
             return {
+                encrypted: false,
                 content: content,
                 signatures: []
             };
@@ -238,6 +240,7 @@ OpenPGPDecrypt.decryptContent = function(content, publicKeys, privateKey, onDecr
     }, function(err) {
         OpenPGPZimbraSecure.popupErrorDialog(err);
         return {
+            encrypted: false,
             content: content,
             signatures: []
         };
@@ -250,6 +253,7 @@ OpenPGPDecrypt.decryptContent = function(content, publicKeys, privateKey, onDecr
             };
             return openpgp.verify(opts).then(function(signature) {
                 return {
+                    encrypted: false,
                     content: signature.data,
                     signatures: signature.signatures
                 };
@@ -261,6 +265,7 @@ OpenPGPDecrypt.decryptContent = function(content, publicKeys, privateKey, onDecr
     }, function(err) {
         OpenPGPZimbraSecure.popupErrorDialog(err);
         return {
+            encrypted: false,
             content: content,
             signatures: []
         };
